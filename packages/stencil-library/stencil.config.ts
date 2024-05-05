32d8928088e7f8e97bd8cd62f063177e301f0639
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-05-05 17:11:57 -0600

    feat: 🎸 Adding right configuration for multi packages

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,31 @@
+import { Config } from '@stencil/core';
+import { vueOutputTarget } from '@stencil/vue-output-target';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    // {
+    //   type: 'dist-custom-elements',
+    //   customElementsExportBehavior: 'auto-define-custom-elements',
+    //   externalRuntime: false,
+    // },
+    // {
+    //   type: 'docs-readme',
+    // },
+    // {
+    //   type: 'www',
+    //   serviceWorker: null, // disable service workers
+    // },
+    vueOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../vue-library/lib/components.ts',
+    }),
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
